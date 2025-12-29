cac6bb8d09829a338b4cf11459cc2b05df39ad7d
Ubuntu <ubuntu@ip-172-31-38-157.eu-north-1.compute.internal>
2025-12-29 05:56:59 +0000

    commiting changes from file diskspace

--- 02.diskspace.sh
+++ 02.diskspace.sh
@@ -2,9 +2,14 @@
 
 FU=$(df -h | egrep -v "Filesystem|tmpfs"| egrep -v "/boot/efi" | grep "/boot" | awk {'print $5'}| tr -d %) 
 
+TO="ev23btb0a36@student.nitw.ac.in"
+
 if [[ $FU -ge 80 ]]
 then 
-	echo " Warning disk space running out: $FU % "
+	echo " Warning disk space running out: $FU % "| mutt -s "Disk space alert!! " $TO
 else 
 	echo " Sufficient disk space: $FU %" 
-fi 
+fi
+
+
+
